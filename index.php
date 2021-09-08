355da913f28b1cb74b61749c6d038cc671372501
saurabh-bhasin <bhasin.it@gmail.com>
2021-09-08 11:55:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html> 
 	<head> 
-	<title>PHP mayank Test</title>
+	<title>PHP Saurabh Test</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
 	</head>
 
 <body> 
 	<h1>PHP 2021 Test</h1>
-		<p><b>  Mayank modi Version9</b></p>
+		<p><b>Saurabh bhasin Version9</b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION2 </h2> 
